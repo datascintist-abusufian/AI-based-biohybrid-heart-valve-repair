4bf15f0d1ad68effa6a9fe21e98268dd155170ae
Researcher <abusufian.tex.cu@gmail.com>
2024-12-29 14:47:47 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/AI-Controlled-Biohybrid-Heart-Valve/'
-})
+  plugins: [react()], // React plugin for Vite
+});
